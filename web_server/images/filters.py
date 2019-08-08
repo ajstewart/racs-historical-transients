77f996f05014a622e4a6d999041f4ee35d44ef1a
Adam Stewart <adam.stewart73@gmail.com>
2019-08-08 18:29:20 +1000

    Working cone search
    Added a cone search to the query form

--- web_server/images/filters.py
+++ web_server/images/filters.py
@@ -4,6 +4,8 @@ import django_filters
 from django_filters.widgets import RangeWidget
 from crispy_forms.bootstrap import InlineField
 from django import forms
+from django_filters.filters import Filter
+from decimal import Decimal
 
 class MyRangeWidget(RangeWidget):
     def __init__(self, from_attrs=None, to_attrs=None, attrs=None):
@@ -13,7 +15,54 @@ class MyRangeWidget(RangeWidget):
             self.widgets[0].attrs.update(from_attrs)
         if to_attrs:
             self.widgets[1].attrs.update(to_attrs)
+            
+class ConeSearchWidget(forms.widgets.MultiWidget):
+    def __init__(self, attrs=None, dt=None, mode=0):  
 
+        _CHOICES = (
+        ('arcsec', 'arcsec'),
+        ('arcmin', 'arcmin'),
+        ('deg', 'deg'),
+        )
+    
+
+        _widgets = (
+            forms.widgets.NumberInput(attrs={"placeholder":"RA (deg)", "step":0.0000000001, "style":"margin:5px"}), 
+            forms.widgets.NumberInput(attrs={"placeholder":"Dec (deg)", "step":0.0000000001, "style":"margin:5px"}),
+            forms.widgets.NumberInput(attrs={"placeholder":"Radius", "step":0.0000000001, "style":"margin:5px"}),
+            forms.widgets.Select(attrs={"placeholder":"Radius", "style":"margin:5px"}, choices = _CHOICES)
+            )
+        super(ConeSearchWidget, self).__init__(_widgets, attrs)
+
+class ConeSearchFilter(Filter):
+    # field_class = forms.DecimalField
+
+    def filter(self, qs, value):
+        print value
+
+        return super()
+
+class MyRangeFilter(Filter):
+    field_class = django_filters.fields.RangeField
+
+    def filter(self, qs, value):
+        if value:
+            if value.start is not None and value.stop is not None:
+                value_start = value.start/Decimal(1000.)
+                value_stop = value.stop/Decimal(1000.)
+                lookup = '%s__range' % self.field_name
+                return self.get_method(qs)(**{lookup: (value_start, value_stop)})
+            else:
+                if value.start is not None:
+                    value_start = value.start/Decimal(1000.)
+                    qs = self.get_method(qs)(**{'%s__gte' % self.field_name: value_start})
+                if value.stop is not None:
+                    value_stop = value.stop/Decimal(1000.)
+                    qs = self.get_method(qs)(**{'%s__lte' % self.field_name: value_stop})
+            if self.distinct:
+                qs = qs.distinct()
+        return qs
+        
 
 class TransientFilter(django_filters.FilterSet):
     
@@ -21,10 +70,27 @@ class TransientFilter(django_filters.FilterSet):
         super(TransientFilter, self).__init__(*args, **kwargs)
         if self.data == {}:
             self.queryset = self.queryset.none()
-        # self.form.fields["transient_type"].widget.attrs['class'] == 
+    
+    def cone_search_filter(self, queryset, name, value):
+        #Don't run if not all value are entered.
+        if '' in value or None in value:
+            return queryset
+        else:
+            ra=float(value[0])
+            dec=float(value[1])
+            radius=float(value[2])
+            unit = value[3]
+            scale_factors = {"arcsec":3600., "arcmin":60.}
+            if unit in scale_factors:
+                radius/=scale_factors[unit]
+            
+            return queryset.extra(where=["q3c_radial_query(ra, dec, {:.6f}, {:.6f}, {:.6f})".format(ra,dec,radius)])
     
     d2d__gt = django_filters.RangeFilter(name = 'd2d_askap_centre', widget=MyRangeWidget(from_attrs={'placeholder': 'Min'}, to_attrs={'placeholder':'Max'}))
     ratio__gt = django_filters.RangeFilter(name = 'ratio', widget=MyRangeWidget(from_attrs={'placeholder': 'Min'}, to_attrs={'placeholder':'Max'}))
+    
+    askap_iflux__gt = MyRangeFilter(name = 'askap_iflux', widget=MyRangeWidget(from_attrs={'placeholder': 'Min (mJy)'}, to_attrs={'placeholder':'Max (mJy)'}), label="ASKAP Integrated Flux")
+    catalog_iflux__gt = MyRangeFilter(name = 'catalog_iflux', widget=MyRangeWidget(from_attrs={'placeholder': 'Min (mJy)'}, to_attrs={'placeholder':'Max (mJy)'}), label="Catalogue Integrated Flux")
     
     pipelinetag_choices = (
         ('Candidate', 'Candidate'),
@@ -61,7 +127,8 @@ class TransientFilter(django_filters.FilterSet):
     
     usertag = django_filters.ChoiceFilter(name = 'usertag', choices=usertag_choices)
     
+    cone_search = ConeSearchFilter(field_name = 'cone_search', widget=ConeSearchWidget(), method="cone_search_filter", label="Cone Search")
+    
     class Meta:
         model = Transients
         fields = []
-        
