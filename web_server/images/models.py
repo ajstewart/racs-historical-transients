1a0b4eff4cadd5a702a08923f7bb83b5b4377f25
Adam Stewart <adam.stewart73@gmail.com>
2019-08-09 18:24:25 +1000

    Queries now working with next and previous
    Next and previous buttons are now working with user queries. Also added a possible associations table to highlight sources found in multiple images

--- web_server/images/models.py
+++ web_server/images/models.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -103,7 +106,16 @@ class Query(models.Model):
     transient_type = models.CharField(max_length=50)
     user_tag = models.CharField(max_length=50)
     user = models.CharField(max_length=50)
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    last_query = models.TextField(max_length=200, blank=True)
 
-    # def get_absolute_url(self):
-    #     return reverse("queries:detail", kwargs={"id": self.id})
-        #return "/queries/%s/" %(self.id)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
